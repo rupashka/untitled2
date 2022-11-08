a74392d592e1eae765c04b0e64870b897fa11d95
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2022-11-08 17:05:05 +0100

    sd

---
+++ src/AVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVery.java
@@ -0,0 +1,6 @@
+public class AVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVeryLongClassAVery {
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
--- src/MainJava.java
+++ src/MainJava.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainJava {
     public static void main(String[] args) {
-
+        List<String> list = new ArrayList<String>();
     }
 }
